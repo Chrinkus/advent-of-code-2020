9cebed38e297f17dc360a74335a7cdc0c4ccbd32
chrinkus <seeschickrun@gmail.com>
2020-12-02 08:19:34 -0600

    Utilized structured bindings!

--- src/2002.cpp
+++ src/2002.cpp
@@ -8,18 +8,14 @@ std::pair<int,int> count_valid_passwords()
     char ch, xx;
     std::string password;
     while (std::cin >> lower >> xx >> upper >> ch >> xx >> password) {
-        // Part 1
         auto n = std::count(std::begin(password), std::end(password), ch);
         if (lower <= n && n <= upper)
             ++count1;
         
-        // Part 2
-        auto f = 0;
-        if (--lower <= password.size() && password[lower] == ch)
-            ++f;
-        if (--upper <= password.size() && password[upper] == ch)
-            ++f;
-        if (f == 1) 
+        --lower;
+        --upper;
+        if ((password[lower] == ch && password[upper] != ch)
+                || (password[upper] == ch && password[lower] != ch))
             ++count2;
     }
 
@@ -28,8 +24,8 @@ std::pair<int,int> count_valid_passwords()
 
 int main()
 {
-    auto results = count_valid_passwords();
+    auto [ part1, part2 ] = count_valid_passwords();
 
-    std::cout << "Part 1: " << results.first << '\n';
-    std::cout << "Part 2: " << results.second << '\n';
+    std::cout << "Part 1: " << part1 << '\n';
+    std::cout << "Part 2: " << part2 << '\n';
 }
