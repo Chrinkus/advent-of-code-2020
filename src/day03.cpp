116335c448f572e524fd4443fddf408e4061a31f
Chrinkus <seeschickrun@gmail.com>
2020-12-06 11:51:35 -0600

    Renaming files led to re-compilation and warnings
    Fixed warnings.

--- src/day03.cpp
+++ src/day03.cpp
@@ -2,14 +2,15 @@
 #include <vector>
 #include <string>
 #include <numeric>
+#include <array>
 
 using Hill = std::vector<std::string>;
-using Slopes = std::vector<std::pair<int,int>>;
+using Slopes = std::array<std::pair<int,int>,5>;
 
 int count_trees(const std::pair<int,int>& slope, const Hill& hill)
 {
     auto [ right, down ] = slope;
-    int trees = 0, x = 0, y = 0;
+    std::size_t trees = 0, x = 0, y = 0;
     while (y < hill.size()) {
         if (hill[y][x] == '#')
             ++trees;
@@ -21,11 +22,10 @@ int count_trees(const std::pair<int,int>& slope, const Hill& hill)
 
 int64_t get_tree_sum_prod(const Slopes& slopes, const Hill& hill)
 {
-    std::vector<int> results (slopes.size());
+    std::vector<int> results;
+    for (const auto& s : slopes)
+        results.push_back(count_trees(s, hill));
 
-    for (auto i = 0; i < slopes.size(); ++i) {
-        results[i] = count_trees(slopes[i], hill);
-    }
     return std::accumulate(std::begin(results), std::end(results), 1,
             std::multiplies<int>());
 }
@@ -35,11 +35,11 @@ int main()
     Hill hill;
     for (std::string line; std::getline(std::cin, line); )
         hill.push_back(line);
-    const Slopes slopes { {1,1}, {3,1}, {5,1}, {7,1}, {1,2} };
+    constexpr Slopes slopes { { {1,1}, {3,1}, {5,1}, {7,1}, {1,2} } };
 
-    auto part1 = count_trees(slopes[1], hill);
+    const auto part1 = count_trees(slopes[1], hill);
     std::cout << "Part 1: " << part1 << '\n';
 
-    auto part2 = get_tree_sum_prod(slopes, hill);
+    const auto part2 = get_tree_sum_prod(slopes, hill);
     std::cout << "Part 2: " << part2 << '\n';
 }
